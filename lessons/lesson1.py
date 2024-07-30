e85e29ba4e25de80696276cb5a6074805060a9f0
dowzwi <akkfaer3@gmail.com>
2024-07-30 20:38:01 +0600

    'lesson1'

---
+++ lessons/lesson1.py
@@ -0,0 +1,12 @@
+
+import lesson1
+import art
+
+
+print(colorama.Back.BLACK
+      ,colorama.Fore.GREEN)
+print('Hello World')
+
+
+
+tprint('Hello World')
---
+++ lessons/test.py
@@ -0,0 +1,5 @@
+from art import tprint
+import test
+import art
+
+tprint('Hello World')
